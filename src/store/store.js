7e03c4d2dc862407443fe6102c7b606558d3b956
Cl1493 <formaciondecarla@gmail.com>
2024-04-16 20:08:58 +0200

    navbar y profile

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,29 @@ import { create } from 'zustand'
 const useGlobalStore = create((set) => ({
     variables:{
         test: 'Hello World',
+        person: {}
+
     },
     functions: {
+
+        /*
+        getProfileInfo: (id) => {
+            fetch(`https://noveltradeback.onrender.com/users/user/${userId}`)
+                .then(response => response.json())
+                .then(data => setStore({ person: }))
+                .catch(error => console.log('Error', error));
+        }
+        */
+
     }
   
 }))
+
+
+
+
+
+
+
 
 export default useGlobalStore;
