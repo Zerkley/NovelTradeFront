3ceeb992ea9db30839edfc7560302a4b39b9a746
zerkley <miguelmr94@gmail.com>
2024-04-24 11:56:12 +0100

    add new variables

--- src/store/store.js
+++ src/store/store.js
@@ -1,130 +1,79 @@
 import { create } from "zustand";
 
 const UseGlobalStore = create((set, get) => ({
-  variables: {
-    test: "Hello World",
-    person: {},
-    usuario: {},
-    token: "",
+    variables: {
+        test: "Hello World",
+        person: {},
+        usuario: {},
+        token: "",
+        userBooks: [],
+        userId: '',
 
-    allNonBooks: [
-      {
-        _id: "661bbcb2ab493d375ee92a97",
-        title: "harry potter",
-        type: "string",
-        state: "string",
-        publishedYear: 0,
-        genre: "string",
-        author: "string",
-        size: "string",
-        picture: "string",
-        owner: "661a9a8cea642e54e378c519",
-        __v: 0,
-      },
-      {
-        _id: "661bf35b7e3850be609a9035",
-        title: "ESDLA",
-        type: "string",
-        state: "string",
-        publishedYear: 0,
-        genre: "string",
-        author: "string",
-        size: "string",
-        picture: "string",
-        owner: "661a9a8cea642e54e378c519",
-        __v: 0,
-      },
-      {
-        _id: "661bbcb2ab493d375ee92a97",
-        title: "100 años de soledad",
-        type: "string",
-        state: "string",
-        publishedYear: 0,
-        genre: "string",
-        author: "string",
-        size: "string",
-        picture: "string",
-        owner: "661a9a8cea642e54e378c519",
-        __v: 0,
-      },
-      {
-        _id: "661bbcb2ab493d375ee92a97",
-        title: "La reina libertad",
-        type: "string",
-        state: "string",
-        publishedYear: 0,
-        genre: "string",
-        author: "string",
-        size: "string",
-        picture: "string",
-        owner: "661a9a8cea642e54e378c519",
-        __v: 0,
-      },
-    ],
+        allNonBooks: [],
 
-    getCrearUsuario: async (email, password) => {
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
+        getCrearUsuario: async (email, password) => {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
 
-      const raw = JSON.stringify({
-        email: email,
-        password: password,
-      });
+            const raw = JSON.stringify({
+                email: email,
+                password: password,
+            });
 
-      const requestCrearUsuario = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow",
-      };
+            const requestCrearUsuario = {
+                method: "POST",
+                headers: myHeaders,
+                body: raw,
+                redirect: "follow",
+            };
 
-      await fetch(
-        "https://noveltradeback.onrender.com/users/signup",
-        requestCrearUsuario
-      )
-        .then((response) => response.json())
-        .then((result) => set({ usuario: result }))
-        .catch((error) => console.log("error", error));
+            await fetch(
+                "https://noveltradeback.onrender.com/users/signup",
+                requestCrearUsuario
+            )
+                .then((response) => response.json())
+                .then((result) => set({ usuario: result }))
+                .catch((error) => console.log("error", error));
+        },
+
+        getLogin: async (email, password) => {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type",
+                "application/json");
+
+            const raw = JSON.stringify({
+                email: email,
+                password: password,
+
+            });
+
+            const requestLogin = {
+                method: "POST",
+                headers: myHeaders,
+                body: raw,
+                redirect: "follow",
+            };
+
+            await fetch(
+                "https://noveltradeback.onrender.com/users/login/",
+                requestLogin
+            )
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("La solicitud no fue exitosa");
+                    }
+                    return response.json();
+                })
+                .then((result) => {
+                    set(state => ({ variables: { ...state.variables, token: result.token, userId: result.userId } }))
+                    console.log(get().variables.token);
+                })
+                .catch((error) => {
+                    console.log("error", error);
+                    alert("Hubo un error al procesar la solicitud");
+                });
+        },
     },
-
-    getLogin: async (email, password) => {
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", 
-        "application/json");
-      
-        const raw = JSON.stringify({
-          email: email,
-          password: password, 
-          
-        });
-      
-        const requestLogin = {
-          method: "POST",
-          headers: myHeaders,
-          body: raw,
-          redirect: "follow",
-        };
-      
-        await fetch(
-          "https://noveltradeback.onrender.com/users/login/",
-          requestLogin
-        )
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("La solicitud no fue exitosa");
-            }
-            return response.json();
-          })
-          .then((result) => {
-            set(state => ({ variables: { ...state.variables, token: result } }))
-            console.log(get().variables.token);
-          })
-          .catch((error) => {
-            console.log("error", error);
-            alert("Hubo un error al procesar la solicitud");
-          });
-      },
-  },
 }));
 
 export default UseGlobalStore;
